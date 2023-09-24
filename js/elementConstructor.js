b644f575929c7cf8f448fd5ee130d769996e925d
beauthebeau <beauthebeau@proton.me>
2023-09-24 18:13:56 +0100

    Removed unneeded `<button>` elements pages of the website, significant change to footer, add testimonails
    `elementConstructor.js`, was cleaned up. Code made more concise and efficient by using helper functions to create nav links.
    
    The changes were done to enhance the speed, efficiency, and readability of the code. Removing superfluous button elements improves web accessibility and overall user experience.

--- js/elementConstructor.js
+++ js/elementConstructor.js
@@ -2,31 +2,18 @@
  * @fileOverview Constructs repeated elements such as the header and footer.
  */
 
+const links = [
+    { href: `./index.html`, name: `Home`, id: `home`, icon: "nf-md-home" },
+    { href: `./download.html`, name: `Download`, id: `downloads`, icon: "nf-md-download" },
+    { href: `./help.html`, name: `Help`, id: `help`, icon: "nf-md-help" },
+    { href: `./credits.html`, name: `Credits`, id: `credits`, icon: "nf-md-crowd" }
+];
+
 /**
  * @name navConstructor
  */
 
 function navConstructor() {
-
-    const links = [
-        { href: `./index.html`, name: `Home`, id: `home`, icon: "nf-md-home" },
-        { href: `./download.html`, name: `Download`, id: `downloads`, icon: "nf-md-download" },
-        { href: `./help.html`, name: `Help`, id: `help`, icon: "nf-md-help" },
-        { href: `./credits.html`, name: `Credits`, id: `credits`, icon: "nf-md-crowd" }
-    ];
-
-    // find every section with an id
-    // const sections = document.querySelectorAll(`section[id]`);
-    // for (let i = 0; i < sections.length; i++) {
-    //     const section = sections[i];
-    //     const link = {
-    //         href: `#${section.id}`,
-    //         name: section.id.charAt(0).toUpperCase() + section.id.slice(1).replace(/-/g, ` `),
-    //         id: section.id
-    //     }
-    //     links.push(link);
-    // }
-
 
     const nav = document.createElement(`nav`);
 
@@ -39,7 +26,6 @@ function navConstructor() {
         const icon = document.createElement(`i`);
         icon.classList.add(`nf`);
         icon.classList.add(links[i].icon);
-        console.log(links[i].icon)
         link.prepend(icon);
 
         nav.appendChild(link);
@@ -49,23 +35,48 @@ function navConstructor() {
 
 }
 
+function createElement(tag, classes = [], content = '') {
+    const element = document.createElement(tag);
+    if (classes.length > 0) element.classList.add(...classes);
+    if (content) element.innerHTML = content;
+    return element;
+}
+
+function createLink(link) {
+    const a = createElement('a', ['footer__link']);
+    a.href = link.href;
+    a.innerText = link.name;
+    a.id = link.id;
+    return a;
+}
+
 function footerConstructor() {
+    const footer = createElement('footer');
 
-    const footer = document.createElement(`footer`);
+    // noinspection HtmlUnknownTarget
+    footer.appendChild(createElement('div', ['logo'], '<img src="img/amogos-logo.png" alt="Logo" class="logo__image">'));
 
-    const h4 = document.createElement(`h4`);
-    h4.innerHTML = `<b>© AmogOS Project</b> | All auspiciousness reserved`;
-    footer.appendChild(h4);
+    // noinspection HtmlUnknownTarget
+    const textContent = `
+        <h2>AmogOS <span id="toppat"><img src="img/toppat.png" alt="Top Hat" id="topHatImage" class="topHatImage">Toppat</span></h2>
+        <p>The sus OS for sus computers is back, and <span class="sussy">even more sussy</span>.</p>
+        <p><strong>© AmogOS Project</strong> All auspiciousness reserved.</p>
+    `;
+    const text = createElement('div', ['footer__text', 'footer__subsection'], textContent);
 
-    const p = document.createElement(`p`);
-    const a = document.createElement(`a`);
-    a.href = `credits.html`;
-    a.innerText = `Our story, team and acknowledgment`;
+    const linksContainer = createElement('div', ['footer__links', 'footer__subsection']);
+    linksContainer.appendChild(createElement('h2', [], 'Links'));
+    links.forEach(link => linksContainer.appendChild(createLink(link)));
 
-    p.appendChild(a);
-    footer.appendChild(p);
+    const social = createElement('div', ['footer__social', 'footer__subsection']);
+    social.appendChild(createElement('h2', [], 'Social'));
+    const discordLink = createLink({ href: 'https://discord.gg/z86muknm9y', id: 'discord', name: 'Join the Discord' });
+    const githubLink = createLink({ href: 'https://github.com/amog-os', id: '', name: 'AmogOS GitHub' });
+    social.appendChild(discordLink);
+    social.appendChild(githubLink);
+
+    footer.append(text, linksContainer, social);
     document.body.appendChild(footer);
-
 }
 
 navConstructor();
