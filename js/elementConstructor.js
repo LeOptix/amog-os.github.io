bd520476a6393a1b2d65c18c643737f6de500300
beauthebeau <beauthebeau@proton.me>
2023-09-24 14:58:30 +0100

    Update navbar creation process in elementConstructor.js
    Expanded the links array to include direct references to Home, Download, Help, and Credits pages along with their respective icons. Commented out code that looks for `section` elements.

--- js/elementConstructor.js
+++ js/elementConstructor.js
@@ -8,19 +8,24 @@
 
 function navConstructor() {
 
-    const links = [ { href: `./index.html`, name: `Home`, id: `home` } ]
+    const links = [
+        { href: `./index.html`, name: `Home`, id: `home`, icon: "nf-md-home" },
+        { href: `./download.html`, name: `Download`, id: `downloads`, icon: "nf-md-download" },
+        { href: `./help.html`, name: `Help`, id: `help`, icon: "nf-md-help" },
+        { href: `./credits.html`, name: `Credits`, id: `credits`, icon: "nf-md-crowd" }
+    ];
 
     // find every section with an id
-    const sections = document.querySelectorAll(`section[id]`);
-    for (let i = 0; i < sections.length; i++) {
-        const section = sections[i];
-        const link = {
-            href: `#${section.id}`,
-            name: section.id.charAt(0).toUpperCase() + section.id.slice(1).replace(/-/g, ` `),
-            id: section.id
-        }
-        links.push(link);
-    }
+    // const sections = document.querySelectorAll(`section[id]`);
+    // for (let i = 0; i < sections.length; i++) {
+    //     const section = sections[i];
+    //     const link = {
+    //         href: `#${section.id}`,
+    //         name: section.id.charAt(0).toUpperCase() + section.id.slice(1).replace(/-/g, ` `),
+    //         id: section.id
+    //     }
+    //     links.push(link);
+    // }
 
 
     const nav = document.createElement(`nav`);
@@ -30,6 +35,13 @@ function navConstructor() {
         link.href = links[i].href;
         link.innerText = links[i].name;
         link.id = links[i].id;
+
+        const icon = document.createElement(`i`);
+        icon.classList.add(`nf`);
+        icon.classList.add(links[i].icon);
+        console.log(links[i].icon)
+        link.prepend(icon);
+
         nav.appendChild(link);
     }
 
